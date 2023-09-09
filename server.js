7ded8d2fc2d0a8de7e0825741c68312ed82dc7db
Tova Greene <tovalgreene@gmail.com>
2023-09-09 17:42:03 -0400

    Edited server.js file

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const path = require('path'); //for node.js
+const session = require('express-session');//express.js
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//HAVE TO SET UP SESSION MIDDLEWARE AND SECURE A SECRET KEY
+//app.use(
+  //session.......
+//)
 //TODO: Don't forget to configure your app to accept JSON
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -17,6 +24,13 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+//app.use(passport.initialize()) -- WHEN MAKE PASSPORT JS
+//app.use(passport.session()) -- WHEN MAKE PASSPORT JS
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
