b16445777ecaff297856062e8c14872ea006468a
Tova Greene <tovalgreene@gmail.com>
2023-09-11 20:26:54 -0400

    Finished authentication

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const passport = require('./config/passport-config');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const path = require('path'); //for node.js
 const session = require('express-session');//express.js
+const dotenv = require('dotenv'); // import .env
+
+dotenv.config(); //load variables from .env
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//HAVE TO SET UP SESSION MIDDLEWARE AND SECURE A SECRET KEY
-//app.use(
-  //session.......
-//)
-//TODO: Don't forget to configure your app to accept JSON
+//Set up middleware with secret key
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+//serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-//TODO: Use the Express Router for more detailed routes
+//parse JSON & URL-encoded request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//initialize passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+//define routes
+app.use(routes);
 
 app.get('/', function (req, res) {
     res.render('home');
 });
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
-//app.use(passport.initialize()) -- WHEN MAKE PASSPORT JS
-//app.use(passport.session()) -- WHEN MAKE PASSPORT JS
-
+//sync database & start server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
       console.log(`App listening on port http://localhost:${PORT}`);
