b84f4ae50e21dfd01d2d193fdcaf4001f5efa0eb
Tova Greene <tovalgreene@gmail.com>
2023-09-18 21:18:36 -0400

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,21 +7,39 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PW;
 const host = process.env.DB_HOST;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-  host: host,
-  dialect: 'mysql',
-  dialectOptions: {
-    decimalNumbers: true,
-  },
-});
+let sequelize;
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection established');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+// const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+//   host: host,
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     decimalNumbers: true,
+//   },
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Database connection established');
+//   })
+//   .catch((err) => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    dbName,
+    dbUser,
+    dbPassword,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
 
 module.exports = sequelize;
